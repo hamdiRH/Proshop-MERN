a4d7bbcfdb6517edaadce17198416f3c7bcb5662
hamdiRH <49039080+hamdiRH@users.noreply.github.com>
2020-11-01 23:38:03 +0100

    prepare to deploy

--- src/server.js
+++ src/server.js
@@ -23,15 +23,24 @@ app.options('*', cors());
 
 app.use(morgan('dev'))
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 
 //** routes
 app.use('/api', routes);
+if (config.env === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
+
 
 const PORT = config.port || 5000;
 app.listen(
